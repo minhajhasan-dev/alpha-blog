b3ae5abaca0e0ebac28cc3490edccb1ddd51c3e0
minhajhasan-dev <minhajhasan.dev@gmail.com>
2026-02-16 08:09:26 +0600

    learned about secure password methods with BCrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_15_192321) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_16_015249) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
@@ -22,6 +22,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_15_192321) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
+    t.string "password_digest"
     t.datetime "updated_at", null: false
     t.string "username"
   end
