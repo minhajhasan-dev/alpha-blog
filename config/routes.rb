28b44ce759e66604dfbc4c8f90f3c69922bb1ec4
minhajhasan-dev <minhajhasan.dev@gmail.com>
2026-02-17 01:29:25 +0600

    added user signup form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root "pages#home"
   get "about", to: "pages#about"
   resources :articles
+  get "signup", to: "users#new"
+  resources :users, except: [ :new ]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
